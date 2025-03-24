074f3f9cb5e0e03b630049b598dab78f099f826e
luqiang <luqiang@muchcloud.com>
2025-03-24 10:40:00 +0800

    init

---
+++ example/android/app/src/main/java/com/firefly/yhcadsdk/firefly_example/MainActivity.java
@@ -0,0 +1,6 @@
+package com.firefly.yhcadsdk.firefly_example;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ ios/Classes/FireflyPlugin.m
@@ -0,0 +1,20 @@
+#import "FireflyPlugin.h"
+
+@implementation FireflyPlugin
++ (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
+  FlutterMethodChannel* channel = [FlutterMethodChannel
+      methodChannelWithName:@"firefly"
+            binaryMessenger:[registrar messenger]];
+  FireflyPlugin* instance = [[FireflyPlugin alloc] init];
+  [registrar addMethodCallDelegate:instance channel:channel];
+}
+
+- (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
+  if ([@"getPlatformVersion" isEqualToString:call.method]) {
+    result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
+  } else {
+    result(FlutterMethodNotImplemented);
+  }
+}
+
+@end
